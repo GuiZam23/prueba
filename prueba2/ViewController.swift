0122c0b690186886e56c187736f55f9b1ecfd990
Jose Flavio Guillen Vera <jflavio@iMac-de-Jose.lan>
2015-11-30 14:06:54 -0600

    Initial Commit

---
+++ prueba2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prueba2
+//
+//  Created by Jose Flavio Guillen Vera on 30/11/15.
+//  Copyright (c) 2015 Jose Flavio Guillen Vera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
